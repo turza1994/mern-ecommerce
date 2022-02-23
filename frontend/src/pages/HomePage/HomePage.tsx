635de88e37a9f9a9156a55a16ca3ce7b54efb621
turza1994 <turza1994@yahoo.com>
2022-02-23 19:38:25 +0600

    config for deployment

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import ShopPreview from '../../components/Home/ShopPreview/ShopPreview';
 import MenuItems from '../../components/MenuItems/MenuItems';
